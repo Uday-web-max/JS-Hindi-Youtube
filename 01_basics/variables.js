0a074e80d0c86d890155e69c2b22315a09cf8483
Uday Kumar <uday.learn247@gmail.com>
2024-09-11 04:47:32 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail ="uday.learn247@gmail.com"
+var accountPassword = "70308"
+accountCity = "Faridabad"
+
+//  accountId = 70308
+
+accountEmail = "uday70308@gmail.com"
+accountPassword = "965087"
+accountCity = "Jaipur"
+
+// we can change let and var but we can't change the value of const
+// highly recommended to only use const and let.
+
+console.table({accountId, accountEmail, accountPassword, accountCity})

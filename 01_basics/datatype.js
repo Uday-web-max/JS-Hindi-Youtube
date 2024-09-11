3954dcf7d607db3695c272816a292c685385c2b4
Uday Kumar <uday.learn247@gmail.com>
2024-09-11 12:43:08 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,10 @@
+
+let accountId = 8726289
+let accountEmail = "uday.learn247@gmail.com"
+let accountPassword = "70308"
+    accountCity = "Faridabad"
+
+    console.log(accountId);
+    
+   
+    

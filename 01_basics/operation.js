c2b76ac55c689454478a53fa270831f6d9f77e4d
Uday Kumar <uday.learn247@gmail.com>
2024-09-12 03:51:21 +0000

    convertion of datatype

---
+++ 01_basics/operation.js
@@ -0,0 +1,29 @@
+let score =  "uday"
+
+    // console.log(typeof (score));
+
+let valueInNumber = Number(score)       
+    // console.log(valueInNumber);
+
+
+    //  "33" => 33
+    //   "33abc" => NaN
+    //   true => 1 , false => 0
+
+
+    let isLoggedIn = "UDAY"
+    let booleanIsLoggedIn = Boolean(isLoggedIn)
+        // console.log(booleanIsLoggedIn);
+        
+
+
+    // 1 => TRUE , 0 => false
+    // "" => false , "uday" => true
+
+
+    let someNumber = 23
+    let stringNumber = String(someNumber)
+        console.log(stringNumber);
+        console.log(typeof stringNumber);
+        
+    
